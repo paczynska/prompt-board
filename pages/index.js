4d9ea17c34b78c6ca375a84d4f08ae9e0dbc7b1c
paczynska <paczynska@gmail.com>
2026-03-17 16:08:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
 
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "ml_default");
+    formData.append("resource_type", "auto");
 
     const res = await fetch("https://api.cloudinary.com/v1_1/ddrasgbno/image/upload", {
       method: "POST",
