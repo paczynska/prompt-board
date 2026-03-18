5738e7936eebbb456d780f94faae7d83d115ce43
paczynska <paczynska@gmail.com>
2026-03-18 11:02:14 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300','400','500','700']
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
